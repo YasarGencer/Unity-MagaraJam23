28051170baf6fc30ef7aa48a1dcfec5c4432705c
kayrakocaeli <83376559+kayrakocaeli@users.noreply.github.com>
2023-11-19 16:48:37 +0300

    Menu done

--- Bard/Assets/Game/Scripts/Managers/GameManager.cs
+++ Bard/Assets/Game/Scripts/Managers/GameManager.cs
@@ -11,13 +11,17 @@ public class GameManager : MonoBehaviour
     bool isInit = false;
     [SerializeField] CanvasGroup pauseScreen, optionsScreen;
     [SerializeField] Slider music, sfx;
+    [SerializeField] Slider musicOptions, sfxOptions;
     bool canPause = true;
     bool isPaused = false;
+    public CanvasGroup canvas;
     public bool IsPaused {  get { return isPaused; } }
     public void Initialize() {
         isInit = true;
         music.onValueChanged.AddListener(MusicVolumeChanged);
         sfx.onValueChanged.AddListener(SFXVolumeChanged);
+        musicOptions.onValueChanged.AddListener(MusicVolumeChanged);
+        sfxOptions.onValueChanged.AddListener(SFXVolumeChanged);
         Pause(false);
     }
 
@@ -46,13 +50,24 @@ public class GameManager : MonoBehaviour
         if(SceneManager.GetActiveScene().buildIndex != 1)
             if (Input.GetKeyDown(KeyCode.Escape))
                 Pause(!isPaused); 
+
     }
+#if UNITY_EDITOR
     private void OnApplicationFocus(bool focus) {
         if (focus == false)
             Pause(true);
     }
-    public void Options(bool value) {
-        optionsScreen.DOFade(value ? 1 : 0, .25f);
+#endif
+    public void Options(bool value)
+    {
+        musicOptions.value = GameManager.MusicVolume;
+        sfxOptions.value = GameManager.SFXVolume;
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            canvas.DOFade(!value ? 1 : 0, .25f); 
+        }
+            optionsScreen.DOFade(value ? 1 : 0, .25f);
+
     }
     public void Pause(bool value) {
         music.value = GameManager.MusicVolume;
