5e35e4d7dce2b8ebba8d34422418cee71b8e0b47
Batuhan Erdin <ejderfora@outlook.com.tr>
2023-11-19 09:13:43 +0300

    bolum 5 teyim, merdiven tırmanma buglı, ve hareketli platformlar küpleri taşımıyor

--- Bard/Assets/Game/Scripts/LevelObjects/WheelRotate.cs
+++ Bard/Assets/Game/Scripts/LevelObjects/WheelRotate.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class WheelRotate : MonoBehaviour
 {
     public float rotationSpeed = 5f;
-
+    public bool working = true;
     // Update is called once per frame
     void Update()
     {
         // Otomatik olarak objeyi döndür
-        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        if (working)
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+        
 
     }
 }
---
+++ Bard/Assets/cubeSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeSpawner : MonoBehaviour
+{
+    public GameObject theCube;
+    public Transform spawnTransform;
+    private GameObject yeniKup;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Deniyor");
+            SpawnCube();
+        }   
+    }
+    public void SpawnCube()
+    {
+        if (yeniKup!=null)
+        {
+            yeniKup.SetActive(false);
+        }
+        Vector3 spawnKonumu = spawnTransform.transform.position;
+
+        // Yeni bir küp spawnla
+        yeniKup = Instantiate(theCube, spawnKonumu, Quaternion.identity);
+
+        // Ýsteðe baðlý olarak yeni küpü B objesinin child'ý yapabilirsiniz
+        yeniKup.transform.parent = spawnTransform.transform;
+        //Instantiate(theCube);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpawnCube();
+        }
+    }
+}
