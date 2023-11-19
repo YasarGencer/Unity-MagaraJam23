3e86f9a68d733baa43136b7a1cf669e78c5ccc9a
Batuhan Erdin <ejderfora@outlook.com.tr>
2023-11-19 17:03:18 +0300

    loadScene codes done

--- Bard/Assets/TriggerLoadFinalSceneScript.cs
+++ Bard/Assets/TriggerLoadFinalSceneScript.cs
@@ -5,22 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class TriggerLoadFinalSceneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //SceneManager.LoadScene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         }
     }
 }
